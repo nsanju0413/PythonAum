468ea4a4a285bdd6148ef131285953cb1ac62cbe
sbuggarg <nsanju013@gmail.com>
2024-01-10 19:42:12 -0600

    sNumber.py

---
+++ pythonProject1/sNumber.py
@@ -0,0 +1,39 @@
+s="S00430643"
+print("S# Number: ",s)
+s="Sfirst_numbernju"
+print("S# Number: ",s)
+
+
+
+#first_numberrithmetic Operfirst_numbertors
+first_number=11
+second_number=2
+
+print("Without using the other variable")
+print("sum=",first_number+second_number)
+print("sub=",first_number-second_number)
+print("mul=",first_number*second_number)
+print("div=",first_number/second_number)
+print("mod=",first_number%second_number)
+print("int div=",first_number//second_number)
+print("Exponent=",first_number**second_number)
+
+
+sum=first_number+second_number
+sub=first_number-second_number
+mul=first_number*second_number
+div=first_number/second_number
+mod=first_number%second_number
+int_div=first_number//second_number
+Exponent=first_number**second_number
+
+
+print("\nWith using the other variable")
+print("sum=",sum
+      ,"\nSub=",sub
+      ,"\nMul=",mul
+      ,"\ndiv=",div
+      ,"\nmod=",mod
+      ,"\nInt Div=",int_div
+      ,"\nExponent=",Exponent)
+
